7fe3701eda9cd21020896e5eea7ed225800b543f
dawit assefa <dawimela@gmail.com>
2025-11-13 22:48:36 +0000

    fix

--- clean_notebooks.py
+++ clean_notebooks.py
@@ -1,9 +1,15 @@
 import nbformat, glob
 
-for f in glob.glob("*.ipynb"):  # or use "**/*.ipynb" for subfolders
+# Recursively find all notebooks in current folder and subfolders
+for f in glob.glob("**/*.ipynb", recursive=True):
     nb = nbformat.read(f, as_version=4)
+    changed = False
     for cell in nb.cells:
         if 'metadata' in cell and 'widgets' in cell['metadata']:
             del cell['metadata']['widgets']
-    nbformat.write(nb, f)
-    print(f"Fixed {f}")
+            changed = True
+    if changed:
+        nbformat.write(nb, f)
+        print(f"Fixed {f}")
+    else:
+        print(f"No fix needed for {f}")
